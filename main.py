040f26c1d06d4ed99ed2d567b600efe19ef04e2e
Mr.BoO <56259930+Master-MonSter@users.noreply.github.com>
2024-10-06 11:07:15 +0330

    start & button_hadler
    + Creating start_command_handler -->(
    . create 2 inline keyboard btns
    . get the way)
    
    +Create a button_handler for get client's answer (CallbackQuary)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import Final
 import logging
+import pandas as pd
 
 from telegram import (
     Update,
@@ -15,9 +16,9 @@ from telegram.ext import (
     MessageHandler,
     InlineQueryHandler,
     CallbackContext,
-    CallbackQueryHandler,
-    Updater
+    CallbackQueryHandler
 )
+
 
 TOKEN: Final= "7425268112:AAGMJVGA56RKIS7iIy5wWG5_cld8JEqHhMA"
 CHANNEL_USERNAME: Final= "@bot_polic"
@@ -35,11 +36,39 @@ logger = logging.getLogger(__name__)
 # **************************************************************** LOG ****************************************************************
 
 
+# Start with selecting a way-adding
+async def start_command_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    # Creating for choosing a way to import PH-nums
+    keyboard = [
+        [InlineKeyboardButton("دستی", callback_data='manual')],
+        [InlineKeyboardButton("فایل", callback_data='file')]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    await update.message.reply_text(
+        'چگونه می‌خواهید شماره‌ها را وارد کنید؟',
+        reply_markup=reply_markup
+    )
+    
+    
+# Client's answer handler
+async def button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    query = update.callback_query
+    await query.answer()
+
+    # Check the user's choice
+    if query.data == 'manual':
+        await query.edit_message_text(text="لطفاً شماره‌ها را یکی یکی وارد کنید:")
+        context.user_data['input_type'] = 'manual'
+    elif query.data == 'file':
+        await query.edit_message_text(text="لطفاً فایل شماره‌ها را ارسال کنید:")
+        context.user_data['input_type'] = 'file'
 
 
 if __name__ == "__main__":
     app = ApplicationBuilder().token(TOKEN).build()
-    # app.add_handler(CommandHandler("start", start_command_handler))
+    app.add_handler(CommandHandler("start", start_command_handler))
+    app.add_handler(CallbackQueryHandler(button_handler))
 
 
     app.run_polling()
